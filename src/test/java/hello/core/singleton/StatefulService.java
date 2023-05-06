e916bf7a4e230c5239390c3c3f3109f682da9313
joo-chang <jooc0311@gmail.com>
2023-05-06 17:11:37 +0900

    singleton test

---
+++ src/test/java/hello/core/singleton/StatefulService.java
@@ -0,0 +1,20 @@
+package hello.core.singleton;
+
+public class StatefulService {
+
+/*    //상태를 유지하는 필드
+    private int price;
+
+    public void order(String name, int price) {
+        System.out.println("name = " + name + " price =" + price);
+        this.price = price; // 문제
+    }*/
+    public int order(String name, int price) {
+        System.out.println("name = " + name + " price =" + price);
+        return price;
+    }
+
+//    public int getPrice() {
+//        return price;
+//    }
+}
