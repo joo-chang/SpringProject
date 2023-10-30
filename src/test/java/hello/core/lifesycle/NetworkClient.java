523608590af6c330bb3c85a790641c2e1ab69f46
joo-chang <jooc0311@gmail.com>
2023-10-30 11:02:34 +0900

    빈 생명 주기 콜백 테스트

--- src/test/java/hello/core/lifesycle/NetworkClient.java
+++ src/test/java/hello/core/lifesycle/NetworkClient.java
@@ -1,13 +1,17 @@
 package hello.core.lifesycle;
 
+import org.springframework.beans.factory.DisposableBean;
+import org.springframework.beans.factory.InitializingBean;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
 public class NetworkClient {
 
     private String url;
     public NetworkClient() {
         System.out.println("url = " + url);
 
-        connect();
-        call("초기화 연결 메세지;");
 
     }
 
@@ -27,5 +31,18 @@ public class NetworkClient {
     // 서비스 종료 시 호출
     public void disconnect() {
         System.out.println("close = " + url);
+    }
+
+    @PostConstruct
+    public void init() throws Exception {
+        System.out.println("NetworkClient.afterPropertiesSet");
+        connect();
+        call("초기화 연결 메세지;");
+    }
+
+    @PreDestroy
+    public void close() throws Exception {
+        System.out.println("NetworkClient.destroy");
+        disconnect();
     }
 }
